bda1c4e52dcebac3fdabb63b35037d60edc76474
jonnxie <1837079753@qq.com>
2023-01-30 22:23:42 +0800

    Refine Engine Design.

--- src/Mesh/Mesh.h
+++ src/Mesh/Mesh.h
@@ -36,10 +36,15 @@ namespace Flow {
         IndexMesh<V>::vertices.resize(4);
         IndexMesh<V>::indices = {0, 1, 2, 0, 0, 2, 3};
         double half_size = size / 2.0;
-        setVertex<V>(&IndexMesh<V>::vertices[0], glm::dvec3(-half_size, -half_size, 0.0f));
-        setVertex<V>(&IndexMesh<V>::vertices[1], glm::dvec3(half_size, -half_size, 0.0f));
-        setVertex<V>(&IndexMesh<V>::vertices[2], glm::dvec3(half_size, half_size, 0.0f));
-        setVertex<V>(&IndexMesh<V>::vertices[3], glm::dvec3(-half_size, half_size, 0.0f));
+        setVertexPosition<V>(&IndexMesh<V>::vertices[0], glm::dvec3(-half_size, -half_size, 0.0f));
+        setVertexPosition<V>(&IndexMesh<V>::vertices[1], glm::dvec3(half_size, -half_size, 0.0f));
+        setVertexPosition<V>(&IndexMesh<V>::vertices[2], glm::dvec3(half_size, half_size, 0.0f));
+        setVertexPosition<V>(&IndexMesh<V>::vertices[3], glm::dvec3(-half_size, half_size, 0.0f));
+
+        setVertexNormal<V>(&IndexMesh<V>::vertices[0], glm::dvec3(0, 0, 1.0f));
+        setVertexNormal<V>(&IndexMesh<V>::vertices[1], glm::dvec3(0, 0, 1.0f));
+        setVertexNormal<V>(&IndexMesh<V>::vertices[2], glm::dvec3(0, 0, 1.0f));
+        setVertexNormal<V>(&IndexMesh<V>::vertices[3], glm::dvec3(0, 0, 1.0f));
     }
 
 } // Flow
--- src/Mesh/Vertex.h
+++ src/Mesh/Vertex.h
@@ -14,26 +14,49 @@ namespace Flow {
         glm::dvec3 pos;
     };
 
+    struct Normal{
+        glm::dvec3 dir;
+    };
+
     template<class V = Point>
-    void setVertex(V* _vertex, glm::dvec3 _pos)
+    void setVertexPosition(V* _vertex, glm::dvec3 _pos)
     {
-        if constexpr(std::is_same_v<Point, V>)
+        if constexpr(std::is_base_of_v<Point, V>)
         {
             Point* p = _vertex;
             p->pos = _pos;
         }
     }
 
-    template<class V = Point>
-    void debugVertex(V* _vertex)
+    template<class N = Normal>
+    void setVertexNormal(N* _vertex, glm::dvec3 _dir)
     {
-        if constexpr(std::is_same_v<Point, V>)
+        if constexpr(std::is_base_of_v<Normal, N>)
+        {
+            Normal* p = _vertex;
+            p->dir = _dir;
+        }
+    }
+
+    template<class V = Point>
+    void debugVertexPosition(V* _vertex)
+    {
+        if constexpr(std::is_base_of_v<Point, V>)
         {
             Point* p = _vertex;
             std::cout << "x: " << p->pos.x << " y: "<< p->pos.y << " z: "<< p->pos.z << std::endl;
         }
     }
 
+    template<class N = Normal>
+    void debugVertexNormal(N* _vertex)
+    {
+        if constexpr(std::is_base_of_v<Normal, N>)
+        {
+            Normal* p = _vertex;
+            std::cout << "x: " << p->dir.x << " y: "<< p->dir.y << " z: "<< p->dir.z << std::endl;
+        }
+    }
 
 
 
